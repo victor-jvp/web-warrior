e11ef773fa91f3c707f0500487ebd76f606f7dca
Victor Véliz <ing.vj15@gmail.com>
2022-10-16 16:38:27 -0400

    Lesson 16 - Images plugin
    - Updated sintax to new images plugin (gatsbyImages)

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,9 @@ module.exports = {
   /* Your site config here */
   plugins: [
     'gatsby-transformer-remark',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,10 +18,18 @@ module.exports = {
         path: `${__dirname}/src/projects/`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
   ],
   siteMetadata: {
     title: 'Web Warrior',
     description: 'Web dev portfolio',
-    copyright: 'This website is copyright 2022 Web Warrior'
+    copyright: 'This website is copyright 2022 Web Warrior',
+    contact: 'vvelizp@gmail.com'
   }
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,50 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 
-export default function Projects() {
+export default function Projects({ data }) {
+  console.log(data);
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects and Websites I've Worked</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <p>Like what you see? Email me at {contact} for a quote!</p>
       </div>
     </Layout>
   )
 }
+
+//Export page query
+export const query = graphql`
+  query ProjectsPage {
+    projects: allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
+      nodes {
+        frontmatter {
+          title
+          stack
+          slug
+        }
+        id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+`
