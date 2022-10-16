ce74a78f43cbe4c9c4b9ea9532db472c52c18d56
Victor Véliz <ing.vj15@gmail.com>
2022-10-16 16:52:33 -0400

    Lesson 17 - Loading images for each project

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
+import { GatsbyImage } from "gatsby-plugin-image"
 
 export default function Projects({ data }) {
   console.log(data);
@@ -16,6 +17,7 @@ export default function Projects({ data }) {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <GatsbyImage image={project.frontmatter.thumb.childImageSharp.gatsbyImageData} />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -37,6 +39,11 @@ export const query = graphql`
           title
           stack
           slug
+          thumb {
+            childImageSharp {
+              gatsbyImageData(layout: FULL_WIDTH)
+            }
+          }
         }
         id
       }
